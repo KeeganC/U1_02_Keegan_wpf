3f5c22b6145c3a02f7ec4ae23071bc07f27fec6d
KeeganC <36234333+KeeganC@users.noreply.github.com>
2018-02-09 09:53:55 -0500

    Add files via upload
    Added files

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿/* Keegan Chan
+ * Feb 7 2018
+ * U1_02_Keegan_wpf
+ * Display a customized greeting.*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace U1_02_Keegan_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShowGreeting_Click(object sender, RoutedEventArgs e)
+        {
+            lblOutput.Content = "Hello, " + entryName.Text + "!";
+        }
+    }
+}
